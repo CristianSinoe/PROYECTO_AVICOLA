1d74d481455c472784bf5114201ca3200d7a9158
Sinoe_Ruiz <zs23004727@gmail.com>
2025-06-10 13:21:26 -0600

    PAGES NEW 01

--- src/app/pages/notfound/notfound.ts
+++ src/app/pages/notfound/notfound.ts
@@ -30,15 +30,15 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, color-mix(in srgb, var(--primary-color), transparent 60%) 10%, var(--surface-ground) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 flex flex-col items-center" style="border-radius: 53px">
                         <span class="text-primary font-bold text-3xl">404</span>
-                        <h1 class="text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2">Not Found</h1>
-                        <div class="text-surface-600 dark:text-surface-200 mb-8">Requested resource is not available.</div>
+                        <h1 class="text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2">UPS! HUBO UN ERROR</h1>
+                        <div class="text-surface-600 dark:text-surface-200 mb-8">EL RECURSO NO ESTA DISPONIBLE</div>
                         <a routerLink="/" class="w-full flex items-center py-8 border-surface-300 dark:border-surface-500 border-b">
                             <span class="flex justify-center items-center border-2 border-primary text-primary rounded-border" style="height: 3.5rem; width: 3.5rem">
                                 <i class="pi pi-fw pi-table !text-2xl"></i>
                             </span>
                             <span class="ml-6 flex flex-col">
-                                <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0 block">Frequently Asked Questions</span>
-                                <span class="text-surface-600 dark:text-surface-200 lg:text-xl">Ultricies mi quis hendrerit dolor.</span>
+                                <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0 block">¿QUÉ PASÓ?</span>
+                                <span class="text-surface-600 dark:text-surface-200 lg:text-xl">PUEDE SER QUE EL CONTENIDO <br> NO ESTE DISPONIBLE <br></span>
                             </span>
                         </a>
                         <a routerLink="/" class="w-full flex items-center py-8 border-surface-300 dark:border-surface-500 border-b">
@@ -46,8 +46,8 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
                                 <i class="pi pi-fw pi-question-circle !text-2xl"></i>
                             </span>
                             <span class="ml-6 flex flex-col">
-                                <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0">Solution Center</span>
-                                <span class="text-surface-600 dark:text-surface-200 lg:text-xl">Phasellus faucibus scelerisque eleifend.</span>
+                                <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0">¿QUÉ PUEDE HACER?</span>
+                                <span class="text-surface-600 dark:text-surface-200 lg:text-xl">INTENTAR MÁS TARDE O CONTACTAR <br> A SU ADMINISTRADOR</span>
                             </span>
                         </a>
                         <a routerLink="/" class="w-full flex items-center mb-8 py-8 border-surface-300 dark:border-surface-500 border-b">
@@ -55,11 +55,11 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
                                 <i class="pi pi-fw pi-unlock !text-2xl"></i>
                             </span>
                             <span class="ml-6 flex flex-col">
-                                <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0">Permission Manager</span>
-                                <span class="text-surface-600 dark:text-surface-200 lg:text-xl">Accumsan in nisl nisi scelerisque</span>
+                                <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0">PERMISOS DE ADMINISTRADOR</span>
+                                <span class="text-surface-600 dark:text-surface-200 lg:text-xl">PUEDE SER QUE ESTE CONTENIDO <br> SOLO ESTE DISPONIBLE <br> PARA EL ADMINISTRADOR </span>
                             </span>
                         </a>
-                        <p-button label="Go to Dashboard" routerLink="/" />
+                        <p-button label="REGRESAR" routerLink="/" />
                     </div>
                 </div>
             </div>
